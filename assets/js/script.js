701e925c8fbf205a17a6145e8c3f206d6e2c6635
Sergii Zelikov <sergii.zelikov@gmail.com>
2016-10-18 00:04:03 +0300

    Profile editor UI fixes
    Signed-off-by: Sergii Zelikov <sergii.zelikov@gmail.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,143 +1,144 @@
 "use strict";
 //angular.module('iWayApp', ['ui.router', 'ngStorage']);
 angular
-    .module('iWayApp', ['ui.router', 'ngStorage', 'satellizer'])
-    .config(['$stateProvider', '$urlRouterProvider', '$authProvider', function ($stateProvider, $urlRouterProvider, $authProvider) {
-        // default state
+	.module('iWayApp', ['ui.router', 'ngStorage', 'satellizer', 'ngAnimate', 'ui.bootstrap'])
+	.config(['$stateProvider', '$urlRouterProvider', '$authProvider', function ($stateProvider, $urlRouterProvider, $authProvider) {
+		// default state
 
-        $stateProvider.state('login', {
-            url: '/login',
-            templateUrl: '/pages/login.html',
-            data: {
-                'noLogin': true
-            }
-        });
+		$stateProvider.state('login', {
+			url: '/login',
+			templateUrl: '/pages/login.html',
+			data: {
+				'noLogin': true
+			}
+		});
 
-        $stateProvider.state('profile', {
-            url: '/profile',
-            templateUrl: '/pages/profile.html'
-        });
+		$stateProvider.state('logout', {
+			url: '/logout',
+			template: '<h1>Buy</h1>',
+			controller: ['$auth', '$state', '$sessionStorage', function ($auth, $state, $sessionStorage) {
+				$auth.logout();
+				$sessionStorage.user = false;
+				$state.go('login');
+			}]
+		});
 
-        $authProvider.facebook({
-            clientId: '194950410944165',
-            // by default, the redirect URI is http://localhost:5000
-            redirectUri: location.origin + location.pathname
-        });
+		$stateProvider.state('nutritionix', {
+			url: '/nutritionix',
+			template: '<h1>Nutritionix</h1>',
+			controller: ['$state', '$sessionStorage', function ($state, $sessionStorage) {
+				// Nutritionix
+				//   search product
+				//   add product to FoodLog
+				//   edit product in FoodLog
+			}]
+		});
+
+		$stateProvider.state('profile', {
+			url: '/profile',
+			templateUrl: '/pages/profile.html'
+		});
+
+		$stateProvider.state('profile_edit', {
+			url: '/profile/edit',
+			templateUrl: '/pages/profile_editor.html'
+		});
+
+		$authProvider.facebook({
+			clientId: '1671085129869272',
+			scope: ['email', 'user_posts'],
+			// by default, the redirect URI is http://localhost:5000
+			redirectUri: location.origin + location.pathname
+		});
+
+		$urlRouterProvider.otherwise('/profile');
     }])
-    .controller('loginCtrl', ['$http', '$sessionStorage', '$rootScope', '$auth', '$scope', function ($http, $sessionStorage, $rootScope, $auth, $scope) {
-        var $this = this;
+	.controller('loginCtrl', ['$http', '$sessionStorage', '$state', '$auth', '$scope', function ($http, $sessionStorage, $state, $auth, $scope) {
+		var $this = this;
 
-        $this.login = function () {
-            $auth
-                .login({
-                    email: $this.email,
-                    password: $this.password
-                })
-                .then(function (response) {
-                    console.log(response.data.token);
-                    $auth.setToken(response.data.token);
-                    $rootScope.$state.go('profile');
-                })
-                .catch(function (response) {
-                    alert(response.data.error);
-                    /*toastr.error(
-                    	'Email or password not correct!', {
-                    		closeButton: true
-                    	}
-                    );*/
-                })
-        };
-        $scope.authenticate = function (provider) {
-            $auth.authenticate(provider)
-                .then(function (response) {
-                    console.debug("success", response);
-                    $rootScope.$state.go('profile');
-                })
-                .catch(function (response) {
-                    console.log("catch", response);
-                });
-        };
+		console.log('loginCtrl', $sessionStorage.user);
+		if ($sessionStorage.user) {
+			$state.go('profile');
+			return;
+		}
+		$this.login = function () {
+			$auth
+				.login({
+					email: $this.email,
+					password: $this.password
+				})
+				.then(function (response) {
+					$auth.setToken(response.data.token);
+					$state.go('profile');
+					$scope.logined = true;
+				})
+				.catch(function (response) {
+					alert(response.data.error);
+				})
+		};
+		$scope.authenticate = function (provider) {
+			$auth.authenticate(provider)
+				.then(function (response) {
+					$auth.setToken(response.data.token);
+					$state.go('profile');
+					$scope.logined = true;
+				})
+				.catch(function (response) {
+					console.log("catch", response);
+				});
+		};
     }])
-    .controller('userCtrl', ['$http', '$sessionStorage', '$rootScope', '$auth', '$scope', function ($http, $sessionStorage, $rootScope, $auth, $scope) {
-        var $this = this;
+	.controller('userCtrl', ['$http', '$sessionStorage', '$state', '$auth', '$scope', function ($http, $sessionStorage, $state, $auth, $scope) {
+		var $this = this;
 
-        $this.login = function () {
-            $auth
-                .login({
-                    email: $this.email,
-                    password: $this.password
-                })
-                .then(function (response) {
-                    console.log(response);
-                    //$auth.setToken(response);
-                    $rootScope.$state.go('profile');
-                })
-                .catch(function (response) {
-                    alert(response.data.error);
-                    /*toastr.error(
-                    	'Email or password not correct!', {
-                    		closeButton: true
-                    	}
-                    );*/
-                })
-        };
-        $scope.authenticate = function (provider) {
-            $auth.authenticate(provider)
-                .then(function (response) {
-                    console.debug("success", response);
-                    $rootScope.$state.go('profile');
-                })
-                .catch(function (response) {
-                    console.log("catch", response);
-                });
-        };
-    }])
-    .service('SessionService', ['$injector', function ($injector) {
-        "use strict";
+		$this.user = $sessionStorage.user;
+		if (!$sessionStorage.user) {
+			$http
+				.get('/user')
+				.then(function (response) {
+					$this.user = $sessionStorage.user = response.data;
+				})
+				.catch(function (response) {
+					//alert(response.data.error);
+					$state.go('login'); // We can show error message 'response.data.error' but we donn't
+				});
+		}
+		if ($scope.logined) {
+			$state.go('profile');
+		}
 
-        this.checkAccess = function (event, toState, toParams, fromState, fromParams) {
-            var $scope = $injector.get('$rootScope'),
-                $sessionStorage = $injector.get('$sessionStorage');
+		$this.saveUser = function () {
+			$http
+				.post('/user/save', {
+					name: $this.user.name,
+					status: $this.user.status
+				})
+				.then(function (response) {
+					$this.user = $sessionStorage.user = response.data;
+					$state.go('profile');
+				})
+				.catch(function (response) {
+					console.log(response);
+				})
+		}
+	}])
+	.controller('menuCollapseCtrl', ['$scope', '$sessionStorage', function ($scope, $sessionStorage) {
+		console.log('menuCollapseCtrl');
+		$scope.isNavCollapsed = true;
+		$scope.logined = Boolean($sessionStorage.user);
+	}])
+	.run(['$rootScope', '$state', '$auth', '$sessionStorage', function ($rootScope, $state, $auth, $sessionStorage) {
+		// Здесь мы будем проверять авторизацию
+		$rootScope.$on('$stateChangeStart',
+			function (event, toState) {
+				var requiredLogin = true;
+				if (toState.data && toState.data.noLogin)
+					requiredLogin = false;
 
-            if (toState.data !== undefined) {
-                if (toState.data.noLogin !== undefined && toState.data.noLogin) {
-                    // если нужно, выполняйте здесь какие-то действия 
-                    // перед входом без авторизации
-                }
-            } else {
-                // вход с авторизацией
-                if ($sessionStorage.user) {
-                    $scope.$root.user = $sessionStorage.user;
-                } else {
-                    // если пользователь не авторизован - отправляем на страницу авторизации
-                    event.preventDefault();
-                    $scope.$state.go('login');
-                }
-            }
-        };
-    }])
-    .run(['$rootScope', '$state', '$stateParams', 'SessionService', '$http', function ($rootScope, $state, $stateParams, SessionService, $http) {
-        $rootScope.$state = $state;
-        $rootScope.$stateParams = $stateParams;
-
-        $rootScope.$root.user = null;
-
-        $http.get('/user')
-            .then(function (response) {
-                //$scope.user = response.data;
-                console.log('Geted user', response.data)
-                $rootScope.$state.go('profile');
-            })
-            .catch(function (response) {
-                $rootScope.$state.go('login'); // We can show error message 'response.data.error' but we donn't
-            });
-
-        //$rootScope.$state.go('profile'); // By default open profile with user information
-
-        // Здесь мы будем проверять авторизацию
-        $rootScope.$on('$stateChangeStart',
-            function (event, toState, toParams, fromState, fromParams, options) {
-                SessionService.checkAccess(event, toState, toParams, fromState, fromParams);
-            }
-        );
+				if (requiredLogin && !$auth.isAuthenticated()) {
+					$sessionStorage.user = false;
+					event.preventDefault();
+					$state.go('login');
+				}
+			});
     }]);
--- gulpfile.js
+++ gulpfile.js
@@ -1,260 +1,310 @@
 "use strict";
 
 var lr = require('tiny-lr'), // Минивебсервер для livereload
-    gulp = require('gulp'),
-    livereload = require('gulp-livereload'), // Livereload для Gulp
-    csso = require('gulp-csso'), // Минификация CSS
-    imagemin = require('gulp-imagemin'), // Минификация изображений
-    uglify = require('gulp-uglify'), // Минификация JS
-    concat = require('gulp-concat'), // Склейка файлов
-    rigger = require('gulp-rigger'),
-    less = require('gulp-less'),
-    fileinclude  =  require('gulp-file-include'),
-    connect = require('connect'),
-    connect_static = require('serve-static'),
-    connect_lv = require('connect-livereload'),
-    server = lr(),
-    request = require('request'),
+	gulp = require('gulp'),
+	livereload = require('gulp-livereload'), // Livereload для Gulp
+	csso = require('gulp-csso'), // Минификация CSS
+	imagemin = require('gulp-imagemin'), // Минификация изображений
+	uglify = require('gulp-uglify'), // Минификация JS
+	concat = require('gulp-concat'), // Склейка файлов
+	rigger = require('gulp-rigger'),
+	less = require('gulp-less'),
+	fileinclude  =  require('gulp-file-include'),
+	connect = require('connect'),
+	connect_static = require('serve-static'),
+	connect_lv = require('connect-livereload'),
+	server = lr(),
+	request = require('request'),
 
-    jwt = require('jwt-simple'),
-    db = require('origindb')('db');
+	jwt = require('jwt-simple'),
+	db = require('origindb')('db');
 
 // CONFIG
 var local_secret = 'NO_SECRET',
-    facebook_secret = 'a3019816d4ccbd40d4dbea41c022297b';
+	facebook_secret = '6156dca792a2155aaaf906780dbf4953';
 
 // functions
 var getUser = function (email) {
-        var users = db('users').object();
+		var users = db('users').object();
 
-        for (var id in users) {
-            if (users[id].email === email) return users[id];
-        }
-        return false;
-    },
-    getUserByID = function (ID) {
-        db('users').get(ID);
-    },
-    checkUser = function (email, password) {
-        //
-    };
+		for (var id in users) {
+			if (users[id].email === email) return users[id];
+		}
+		return false;
+	},
+	getUserByID = function (ID) {
+		return db('users').get(ID, {});
+	},
+	checkUser = function (email, password) {
+		//
+	};
 
 
 gulp.task('default', function () {
-    // place code for your default task 
-    console.log("Hello World!");
+	// place code for your default task 
+	console.log("Hello World!");
 
-    db('users').set('user_0', {
-        id: 'user_0',
-        provider: 'local',
-        email: "test@mail.com",
-        name: "Test Man",
-        password: "password",
-        picture: ''
-    }).set('length', 1);
+	db('users').set('user_0', {
+		id: 'user_0',
+		provider: 'local',
+		email: "test@mail.com",
+		name: "Test Man",
+		password: "password",
+		picture: ''
+	}).set('length', 1);
 
-    console.log(getUser('test@mail.com'));
+	console.log(getUser('test@mail.com'));
 });
 
 gulp.task('less', function () {
-    return gulp.src('./assets/less/*.less')
-        .pipe(less({
-            paths: ['./node_modules/bootstrap/less']
-        }))
-        .pipe(gulp.dest('./assets/css'));
+	return gulp.src('./assets/less/*.less')
+		.pipe(less({
+			paths: ['./node_modules/bootstrap/less']
+		}))
+		.pipe(gulp.dest('./assets/css'));
 });
 
 gulp.task('style', function () {
-    gulp.src('./assets/css/**/*.css')
-        .pipe(concat('style.css')) // собираем stylus
-        .pipe(csso())
-        .pipe(gulp.dest('./public/css/')) // записываем css
-        .pipe(livereload(server)); // даем команду на перезагрузку css
+	gulp.src('./assets/css/**/*.css')
+		.pipe(concat('style.css')) // собираем stylus
+		.pipe(csso())
+		.pipe(gulp.dest('./public/css/')) // записываем css
+		.pipe(livereload(server)); // даем команду на перезагрузку css
 });
 
 // Собираем html
 gulp.task('html', function () {
-    gulp.src(['./assets/template/**/*.html', '!./assets/template/**/_*.html'])
-        .pipe(fileinclude({     
-            prefix: '@@',
-            basepath: '@file'
-        })) // Собираем HTML только в папке ./assets/template/ исключая файлы с _*
-        .pipe(gulp.dest('./public/')) // Записываем собранные файлы
-        .pipe(livereload(server)); // даем команду на перезагрузку страницы
+	gulp.src(['./assets/template/**/*.html', '!./assets/template/**/_*.html'])
+		.pipe(fileinclude({     
+			prefix: '@@',
+			basepath: '@file'
+		})) // Собираем HTML только в папке ./assets/template/ исключая файлы с _*
+		.pipe(gulp.dest('./public/')) // Записываем собранные файлы
+		.pipe(livereload(server)); // даем команду на перезагрузку страницы
 });
 
 // Собираем JS
 gulp.task('js', function () {
-    gulp.src([
+	gulp.src([
             './node_modules/angular/angular.js',
+            './node_modules/angular-animate/angular-animate.js',
             './node_modules/angular-ui-router/release/angular-ui-router.js',
+            './node_modules/angular-ui-bootstrap/dist/ui-bootstrap.js',
             './node_modules/satellizer/dist/satellizer.js',
             './node_modules/restangular/dist/restangular.js',
             './node_modules/ngstorage/ngStorage.js',
             './assets/js/**/*.js',
             '!./assets/js/vendor/**/*.js'
         ])
-        .pipe(concat('index.js')) // Собираем все JS, кроме тех которые находятся в ./assets/js/vendor/**
-        /*.pipe(uglify({
+		.pipe(concat('index.js')) // Собираем все JS, кроме тех которые находятся в ./assets/js/vendor/**
+		/*.pipe(uglify({
 			mangle: false
 		}))*/
-        .pipe(gulp.dest('./public/js'))
-        .pipe(livereload(server)); // даем команду на перезагрузку страницы
+		.pipe(gulp.dest('./public/js'))
+		.pipe(livereload(server)); // даем команду на перезагрузку страницы
 });
 
 // Копируем и минимизируем изображения
 gulp.task('images', function () {
-    gulp.src('./assets/img/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./public/img'))
+	gulp.src('./assets/img/**/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('./public/img'))
 });
 
 // Локальный сервер для разработки
 gulp.task('http-server', function () {
-    connect()
-        .use('/auth', function (req, res, next) {
-            switch (req.url) {
-            case '/facebook':
-                var data = '';
-                req.on('data', function (chunk) {
-                    data += chunk.toString();
-                });
-                req.on('end', function () {
-                    data = JSON.parse(data);
-                    var access_token_url = 'https://graph.facebook.com/v2.3/oauth/access_token',
-                        graph_api_url = 'https://graph.facebook.com/v2.5/me?fields=id,name,picture,email',
-                        params = {
-                            client_id: data.clientId,
-                            redirect_uri: data.redirectUri,
-                            client_secret: facebook_secret,
-                            code: data.code
-                        },
-                        access_token,
-                        user_data = {},
-                        payload = {
-                            iss: 'local',
-                            expires: Math.round((new Date().getTime() / 1000)) + 3600
-                        };
+	connect()
+		.use('/auth', function (req, res, next) {
+			switch (req.url) {
+			case '/facebook':
+				var data = '';
+				req.on('data', function (chunk) {
+					data += chunk.toString();
+				});
+				req.on('end', function () {
+					data = JSON.parse(data);
+					var access_token_url = 'https://graph.facebook.com/v2.3/oauth/access_token',
+						graph_api_url = 'https://graph.facebook.com/v2.5/me?fields=id,name,picture,email',
+						graph_api_url_p = 'https://graph.facebook.com/v2.8/me/feed',
+						params = {
+							client_id: data.clientId,
+							redirect_uri: data.redirectUri,
+							client_secret: facebook_secret,
+							code: data.code
+						},
+						access_token,
+						user_data = {},
+						payload = {
+							iss: 'local',
+							expires: Math.round((new Date().getTime() / 1000)) + 3600
+						};
 
-                    request.get({
-                        url: access_token_url,
-                        useQuerystring: true,
-                        qs: params
-                    }, function (e, r, body) {
-                        var access_token = JSON.parse(body);
-                        request.get({
-                            url: graph_api_url,
-                            useQuerystring: true,
-                            qs: access_token
-                        }, function (e, r, body) {
-                            var fb_res = JSON.parse(body),
-                                count = db('users').get('length', 0),
-                                user = getUser(fb_res.email);
-                            console.log('User', fb_res, user, count);
+					request.get({
+						url: access_token_url,
+						useQuerystring: true,
+						qs: params
+					}, function (e, r, body) {
+						var access_token = JSON.parse(body);
+						request.get({
+							url: graph_api_url,
+							useQuerystring: true,
+							qs: access_token
+						}, function (e, r, body) {
+							var fb_res = JSON.parse(body),
+								count = db('users').get('length', 0),
+								user = getUser(fb_res.email),
+								payload = {
+									iss: 'facebook',
+									expires: Math.round((new Date().getTime() / 1000)) + 3600
+								};
 
-                            if (!user) {
-                                user = {
-                                    id: 'user_' + count,
-                                    provider: 'facebook',
-                                    name: fb_res.name,
-                                    email: fb_res.email,
-                                    password: '',
-                                    picture: '',
-                                    facebook: {
-                                        id: fb_res.id,
-                                        token: access_token
-                                    }
-                                };
-                                db('users').set('user_' + count, user);
-                                db('users').set('length', count + 1)
-                            } else {
-                                // update user
-                            }
-                            res.writeHead(200);
-                            res.write('hi');
-                            res.end();
-                        });
-                    });
-                });
-                break;
-            case '/login':
-                var data = '';
-                req.on('data', function (chunk) {
-                    data += chunk.toString();
-                });
-                req.on('end', function () {
-                    var post = JSON.parse(data || '{}'),
-                        users = db('users').object(),
-                        user = getUser(post.email),
-                        user_data = {},
-                        payload = {
-                            iss: 'local',
-                            expires: Math.round((new Date().getTime() / 1000)) + 3600
-                        };
+							console.log('User', fb_res, user, count);
 
-                    if (user && user.password === post.password) {
-                        res.writeHead(200);
-                        payload.ID = user.id;
-                        user_data.name = user.name;
-                        user_data.token = jwt.encode(payload, local_secret, 'HS256');
-                        res.write(JSON.stringify(user_data));
-                    } else {
-                        res.writeHead(404);
-                        res.write('{"error":"Wrong user or password"}');
-                    }
-                    res.end();
-                });
-                break;
-            default:
-                next();
-            }
-        })
-        .use('/user', function (req, res, next) {
-            if (Boolean(req.headers.authorization)) {
-                var token = req.headers.authorization.split(' ')[1],
-                    payload;
-                try {
-                    payload = jwt.decode(token, local_secret);
-                    res.writeHead(200);
-                    res.write(JSON.stringify(getUserByID(payload.ID)));
-                } catch (error) {
-                    console.log(error);
-                    res.writeHead(401);
-                }
-                res.end();
-            } else {
-                res.writeHead(401);
-                res.write('{"error":"Authorization header missing"}');
-                res.end();
-            }
-        })
-        .use(connect_lv())
-        .use(connect_static('./public'))
-        .listen('9000');
+							request.get({
+								url: graph_api_url_p,
+								useQuerystring: true,
+								qs: access_token
+							}, function (e, r, body) {
+								var feed = JSON.parse(body);
+								if (!user) {
+									user = {
+										id: 'user_' + count,
+										provider: 'facebook',
+										name: fb_res.name,
+										email: fb_res.email,
+										password: '',
+										picture: fb_res.picture.data.url,
+										status: '',
+										facebook: {
+											id: fb_res.id,
+											token: access_token
+										}
+									};
 
-    console.log('Server listening on http://localhost:9000');
+									for (var i in feed.data) {
+										if (feed.data[i].message) {
+											user.status = feed.data[i].message;
+											break;
+										}
+									}
+									db('users').set('user_' + count, user);
+									db('users').set('length', count + 1)
+								} else {
+									user.facebook = {
+										id: fb_res.id,
+										token: access_token
+									};
+								}
+
+								user_data.id = user.id;
+								user_data.name = user.name;
+								user_data.image = user.picture;
+								user_data.status = user.status;
+
+								payload.ID = user.id;
+								user_data.token = jwt.encode(payload, local_secret, 'HS256');
+
+								console.log("feed", feed);
+								res.writeHead(200);
+								res.write(JSON.stringify(user_data));
+								res.end();
+							});
+						});
+					});
+				});
+				break;
+			case '/login':
+				var data = '';
+				req.on('data', function (chunk) {
+					data += chunk.toString();
+				});
+				req.on('end', function () {
+					var post = JSON.parse(data || '{}'),
+						users = db('users').object(),
+						user = getUser(post.email),
+						user_data = {},
+						payload = {
+							iss: 'local',
+							expires: Math.round((new Date().getTime() / 1000)) + 3600
+						};
+
+					if (user && user.password === post.password) {
+						res.writeHead(200);
+						payload.ID = user.id;
+						user_data.name = user.name;
+						user_data.token = jwt.encode(payload, local_secret, 'HS256');
+						res.write(JSON.stringify(user_data));
+					} else {
+						res.writeHead(404);
+						res.write('{"error":"Wrong user or password"}');
+					}
+					res.end();
+				});
+				break;
+			default:
+				next();
+			}
+		})
+		.use('/user', function (req, res, next) {
+			var data = '';
+			req.on('data', function (chunk) {
+				data += chunk.toString();
+			});
+			req.on('end', function () {
+				var post = JSON.parse(data || '{}');
+
+				if (Boolean(req.headers.authorization)) {
+					var token = req.headers.authorization.split(' ')[1],
+						payload,
+						user,
+						user_data = {};
+					try {
+						payload = jwt.decode(token, local_secret);
+						user = getUserByID(payload.ID);
+
+						switch (req.url) {
+						case '/save':
+							console.log(post, payload);
+							user.name = post.name;
+							user.status = post.status;
+							db('users').set([payload.ID, 'name'], user.name);
+							db('users').set([payload.ID, 'status'], user.status);
+						}
+						user_data.name = user.name;
+						user_data.image = user.picture || 'img/bw-anonymous-person.png';
+						user_data.status = user.status || '';
+
+						res.writeHead(200);
+						res.write(JSON.stringify(user_data));
+					} catch (error) {
+						console.log('error', error);
+						res.writeHead(401);
+					}
+					res.end();
+				} else {
+					res.writeHead(401);
+					res.write('{"error":"Authorization header missing"}');
+					res.end();
+				}
+			});
+		})
+		.use(connect_lv())
+		.use(connect_static('./public'))
+		.listen('9000');
+
+	console.log('Server listening on http://localhost:9000');
 });
 
 // Запуск сервера разработки gulp watch
 gulp.task('watch', ['less', 'style', 'html', 'images', 'js', 'http-server'], function () {
-    // Подключаем Livereload
-    server.listen(35729, function (err) {
-        if (err) return console.log(err);
+	// Подключаем Livereload
+	server.listen(35729, function (err) {
+		if (err) return console.log(err);
 
-        gulp.watch('assets/less/*.less', function () {
-            gulp.run('less');
-        });
-        gulp.watch('assets/css/**/*.css', function () {
-            gulp.run('style');
-        });
-        gulp.watch('assets/template/**/*.html', function () {
-            gulp.run('html');
-        });
-        gulp.watch('assets/img/**/*', function () {
-            gulp.run('images');
-        });
-        gulp.watch('assets/js/**/*', function () {
-            gulp.run('js');
-        });
-    });
+		gulp.watch('assets/less/*.less', ['less']);
+		gulp.watch('assets/css/**/*.css', ['style']);
+		gulp.watch('assets/template/**/*.html', ['html']);
+		gulp.watch('assets/img/**/*', ['images']);
+		gulp.watch('assets/js/**/*', ['js']);
+	});
 });
